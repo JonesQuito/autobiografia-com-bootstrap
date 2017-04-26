748836fda30b84fdb3769199026950b5b48cd48a
JonesQuito <jonesdhy@hotmail.com>
2017-04-26 00:19:33 -0300

    versão final

--- src/main/webapp/scripts/functions.js
+++ src/main/webapp/scripts/functions.js
@@ -2,14 +2,14 @@
 function resizeDown(){
   divPrincipal = document.getElementById("biografia");
   divPrincipal.style.backgroundColor = 'rgba(0,0,0,1)';
-  divPrincipal.style.margin = '30px 20%';
+  divPrincipal.style.margin = '10px 5%';
   divPrincipal.style.transition = '0.5s';
 }
 
 function resizeUp(){
   divPrincipal = document.getElementById("biografia");
-  divPrincipal.style.backgroundColor = 'rgba(0,0,0,.5)';
-  divPrincipal.style.margin = '10px 10%';
+  divPrincipal.style.backgroundColor = 'rgba(0,0,0,.8';
+  divPrincipal.style.margin = '0px';
   divPrincipal.style.transition = '0.5s';
 }
 
